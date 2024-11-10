f7e6eed42df63c318b41a7d0423d8e33c9c0aea1
Manolache Mihai <mihaimanolache03@gmail.com>
2024-11-10 11:31:36 +0200

    Sonarqube #1

--- Application/DTOs/ProductDto.cs
+++ Application/DTOs/ProductDto.cs
@@ -5,8 +5,8 @@ namespace Application.DTOs
     public class ProductDto
     {
         public Guid ProductId { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
         public decimal Price { get; set; }
         public int Stock { get; set; }
         public Category Category { get; set; }
