24fe36c92efe7544f0e99195bb93c8680237a83e
vanillaxqz <vanillaxyz@protonmail.com>
2024-12-08 16:11:10 +0200

    fixed filtered product query #2

--- Domain/Repositories/IProductRepository.cs
+++ Domain/Repositories/IProductRepository.cs
@@ -6,7 +6,7 @@ namespace Domain.Repositories
     public interface IProductRepository
     {
         Task<Result<Product>> GetProductByIdAsync(Guid productId);
-        Task<Result<IEnumerable<Product>>> GetAllProductsAsync();
+        Task<Result<IEnumerable<Product>>> GetAllProductsAsync(Func<IQueryable<Product>, IQueryable<Product>>? query = null);
         Task<Result<Guid>> AddProductAsync(Product product);
         Task UpdateProductAsync(Product product);
         Task DeleteProductAsync(Guid productId);
