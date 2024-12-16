243a8bfd109bc368d932a7f88017dce7c4414487
Manolache Mihai <mihaimanolache03@gmail.com>
2024-12-16 15:41:36 +0200

    Added the first iteration of the ml part of the project

---
+++ Application/AIML/ProductData.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Microsoft.ML.Data;
+
+namespace Application.AIML
+{
+    public class ProductData
+    {
+        [LoadColumn(0)]
+        public string? Name { get; set; }
+        [LoadColumn(1)]
+        public string? Description { get; set; }
+        [LoadColumn(2)]
+        public float Price { get; set; }
+        [LoadColumn(3)]
+        public int Stock { get; set; }
+        [LoadColumn(4)]
+        public int Category { get; set; }
+    }
+}
---
+++ Application/AIML/ProductPricePredictionModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+
+namespace Application.AIML
+{
+    public class ProductPricePredictionModel
+    {
+        private readonly MLContext mlContext;
+        private ITransformer model;
+        public ProductPricePredictionModel() => mlContext = new MLContext();
+
+        public void TrainModel()
+        {
+            string csvFilePath = "products.csv";
+            var dataView = mlContext.Data.LoadFromTextFile<ProductData>(csvFilePath, hasHeader: true, separatorChar: ',');
+            var pipeline = mlContext.Transforms.Text.FeaturizeText("NameFeaturized", nameof(ProductData.Name))
+                .Append(mlContext.Transforms.Text.FeaturizeText("DescriptionFeaturized", nameof(ProductData.Description)))
+                .Append(mlContext.Transforms.Conversion.ConvertType(nameof(ProductData.Stock), outputKind: DataKind.Single))
+                .Append(mlContext.Transforms.Conversion.ConvertType(nameof(ProductData.Category), outputKind: DataKind.Single))
+                .Append(mlContext.Transforms.Concatenate("Features",
+                    "NameFeaturized", "DescriptionFeaturized", nameof(ProductData.Stock), nameof(ProductData.Category)))
+                .Append(mlContext.Regression.Trainers.Sdca(labelColumnName: nameof(ProductData.Price), maximumNumberOfIterations: 300));
+            model = pipeline.Fit(dataView);
+        }
+
+        public float PredictPrice(ProductData productData)
+        {
+            var predictionEngine = mlContext.Model.CreatePredictionEngine<ProductData, ProductDataPrediction>(model);
+            var prediction = predictionEngine.Predict(productData);
+            return prediction.Price;
+        }
+    }
+}
