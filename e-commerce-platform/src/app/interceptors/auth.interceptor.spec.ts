35ff8c99c3e8acfcc25e33e608272973870c8a8a
Roman Tudor <romantudor.contact@gmail.com>
2025-01-14 01:08:09 +0200

    Added Auth Interceptor Tests
    I have added tests for the Auth interceptor.

--- e-commerce-platform/src/app/interceptors/auth.interceptor.spec.ts
+++ e-commerce-platform/src/app/interceptors/auth.interceptor.spec.ts
@@ -1,17 +1,68 @@
-// import { TestBed } from '@angular/core/testing';
-// import { HttpInterceptorFn } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
+import { AuthInterceptor } from './auth.interceptor';
+import { StorageService } from '../services/storage.service';
 
-// import { authInterceptor } from './auth.interceptor';
+describe('AuthInterceptor', () => {
+  let httpMock: HttpTestingController;
+  let httpClient: HttpClient;
+  let storageService: jasmine.SpyObj<StorageService>;
 
-// describe('authInterceptor', () => {
-//   const interceptor: HttpInterceptorFn = (req, next) =>
-//     TestBed.runInInjectionContext(() => authInterceptor(req, next));
+  beforeEach(() => {
+    const storageServiceSpy = jasmine.createSpyObj('StorageService', ['getItem']);
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//   });
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+        { provide: StorageService, useValue: storageServiceSpy }
+      ]
+    });
 
-//   it('should be created', () => {
-//     expect(interceptor).toBeTruthy();
-//   });
-// });
+    httpMock = TestBed.inject(HttpTestingController);
+    httpClient = TestBed.inject(HttpClient);
+    storageService = TestBed.inject(StorageService) as jasmine.SpyObj<StorageService>;
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should add Authorization header to API requests', () => {
+    storageService.getItem.and.returnValue('test-token');
+
+    httpClient.get('/api/v1/test').subscribe(response => {
+      expect(response).toBeTruthy();
+    });
+
+    const httpRequest = httpMock.expectOne('/api/v1/test');
+
+    expect(httpRequest.request.headers.has('Authorization')).toBeTrue();
+    expect(httpRequest.request.headers.get('Authorization')).toBe('Bearer test-token');
+  });
+
+  it('should not add Authorization header to non-API requests', () => {
+    storageService.getItem.and.returnValue('test-token');
+
+    httpClient.get('/other/test').subscribe(response => {
+      expect(response).toBeTruthy();
+    });
+
+    const httpRequest = httpMock.expectOne('/other/test');
+
+    expect(httpRequest.request.headers.has('Authorization')).toBeFalse();
+  });
+
+  it('should not add Authorization header to reset-password requests', () => {
+    storageService.getItem.and.returnValue('test-token');
+
+    httpClient.get('/api/v1/reset-password').subscribe(response => {
+      expect(response).toBeTruthy();
+    });
+
+    const httpRequest = httpMock.expectOne('/api/v1/reset-password');
+
+    expect(httpRequest.request.headers.has('Authorization')).toBeFalse();
+  });
+});
